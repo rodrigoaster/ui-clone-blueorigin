1f7abe00d99adda072c46ac5cca6abb088edc141
Rodrigo Aster <rodrigowebx@gmail.com>
2022-12-07 15:22:25 -0300

    OverlayDefault: Component structure finalized

--- src/components/OverlayDefault/index.tsx
+++ src/components/OverlayDefault/index.tsx
@@ -1,25 +1,29 @@
-import { Container } from './styles';
+import * as S from './styles';
 
 interface OverlayDefaultProps {
   firstTitle: string;
   secondTitle: string;
+  description: string;
   buttonText: string;
 }
 
-export function OverlayDefault({ firstTitle, secondTitle, buttonText }: OverlayDefaultProps) {
+export function OverlayDefault({ firstTitle, secondTitle, buttonText, description }: OverlayDefaultProps) {
   return (
-    <Container>
-      <div>
+    <S.Container>
+      <S.ContainerBlock>
         <h2>
           {firstTitle}
         </h2>
-        <h2>
+        <h3>
           {secondTitle}
-        </h2>
-        <button>
+        </h3>
+        <p>
+          {description}
+        </p>
+        <S.Button>
           {buttonText}
-        </button>
-      </div>
-    </Container>
+        </S.Button>
+      </S.ContainerBlock>
+    </S.Container>
   );
 }
--- src/components/OverlayDefault/styles.ts
+++ src/components/OverlayDefault/styles.ts
@@ -2,5 +2,30 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     height: 100vh;
-    text-align: center;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
+
+export const ContainerBlock = styled.div`
+    > h2 {
+        font-size: 75px;
+    }
+
+    > h3 {
+        font-size: 75px;
+    }
+` 
+
+export const Button = styled.button`
+    border: 1px solid #ccc;
+    
+
+    padding: 15px 45px;
+
+    text-transform: uppercase;
+    color: #fff;
+
+    font-weight: bold;
+` 
