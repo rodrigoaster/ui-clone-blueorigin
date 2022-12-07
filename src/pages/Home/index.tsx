efefff64fe84f9d720040cb66fc836d5295fbcfc
Rodrigo Aster <rodrigowebx@gmail.com>
2022-12-07 15:23:11 -0300

    Home: Page structure completed, waiting animations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,20 +11,21 @@ export function Home() {
         <div>
           {
             [
-              "FIRST SECTION",
-              "SECOND SECTION",
-              "THIRD SECTION",
-              "FOUR SECTION",
-              "FIVE SECTION",
-            ].map(
-              sectionName => (
+              {title: 'BUILD THE FUTURE', titleTwo: 'JOIN TEAM BLUE', descriptionText: '', btnText: 'Learn More'},
+              {title: 'FLY TO SPACE ON', titleTwo: 'NEW SHEPARD', descriptionText: '', btnText: 'Become an Astronaut'},
+              {title: 'NEW GLENN', titleTwo: 'ORBITAL ROCKET', descriptionText: '', btnText: 'Explore New Glenn'},
+              {title: 'REUSABLE', titleTwo: 'ROCKET ENGINES', descriptionText: "Blue Origin's engines are powering the next generation of rockets for commercial, civil, national security and human spaceflight.", btnText: 'Explore Engines'},
+              {title: 'INTRODUCING', titleTwo: 'ORBITAL REEF', descriptionText: '', btnText: 'Meet Orbital Reef'},
+            ].map(sectionName => (
               <Section 
-                key={sectionName}
+                key={sectionName.title}
+                className='bg'
                 children={
                   <OverlayDefault 
-                    firstTitle='Teste1'
-                    secondTitle='Teste2'
-                    buttonText='TesteButton'
+                    firstTitle={sectionName.title}
+                    secondTitle={sectionName.titleTwo}
+                    description={sectionName.descriptionText}
+                    buttonText={sectionName.btnText}
                   />
                 }
               />
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,92 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    .bg:nth-child(1) {
+        display: flex;
+        justify-content: flex-start;
+        
+        background: #6F1A07;
+        
+        padding-left: 120px;
 
+        h2 {
+            font-weight: lighter;
+            width: 70%;
+            line-height: 1;
+        }
+
+        color: #fff;
+
+    }
+    .bg:nth-child(2) {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+
+        background: #93E5AB;
+        
+        h2 {
+            font-weight: lighter;
+            line-height: 0.5;
+        }
+
+        color: #fff;
+
+    }
+    .bg:nth-child(3) {
+        display: flex;
+        justify-content: flex-start;
+
+        background: #65B891;
+
+        padding-left: 120px;
+
+        h2 {
+            line-height: 0.5;
+        }
+
+        h3 {
+            font-weight: lighter;
+        }
+
+        color: #fff;
+
+    }
+    .bg:nth-child(4) {
+        background: #4E878C;
+
+        h2 {
+            font-weight: lighter;
+            width: 70%;
+            line-height: 0.5;
+        }
+
+        p {
+            font-size: 20px;
+            font-weight: normal;
+
+            width: 60%;
+
+            margin-bottom: 20px;
+        }
+
+        color: #fff;
+
+        padding-left: 50px;
+
+    }
+    .bg:nth-child(5) {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+
+        background: #00241B;
+
+        h2 {
+            font-weight: lighter;
+            line-height: 0.5;
+        }
+
+        color: #fff;
+    }
 `;
