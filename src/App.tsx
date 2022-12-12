d4739a552445b927c78bd55da0b7a3c6f0850454
rodrigoaster <rodrigowebx@gmail.com>
2022-12-12 02:11:19 -0300

    App: Animations after open the page Home.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion";
 import { Home } from "./pages/Home";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 export function App() {
   return (
     <>
-      <Home />
+        <motion.div
+            animate={{ opacity: [0, 1] }}
+            transition={{ delay: 0.5 }}
+        >
+            <Home />
+        </motion.div>
       <GlobalStyles />
     </>
   )
