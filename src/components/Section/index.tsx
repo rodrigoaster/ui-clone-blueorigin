a6876ae8966a3c6949dae6732f888163e7abcdb7
rodrigoaster <rodrigowebx@gmail.com>
2022-12-12 02:12:31 -0300

    Section: Animations of sections implemented.

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,15 +1,30 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
 import { Container } from './styles';
+import { motion } from "framer-motion";
 
 interface SectionProps extends React.HTMLAttributes<HTMLDivElement>{
-  children: ReactNode;
+    overlayNode: ReactNode;
 }
 
-export function Section({ children, ...props}: SectionProps) {
-  return (
-    <Container {...props}>
-      {children}
-    </Container>
+
+export function Section({  overlayNode, ...props}: SectionProps) {
+    const variants = {
+        first: { opacity: 0, y: 0 },
+        last: { opacity: 1, y: -20 }
+    }
+
+    return (
+      <Container {...props}>
+        <motion.div
+            initial="first"
+            whileInView="last"
+            viewport={{ once: true, amount: 0.8 }}
+            variants={variants}
+            transition={{ type: "spring", stiffness: 50 }}
+            >
+            {overlayNode}
+        </motion.div>
+      </Container>
   );
 }
