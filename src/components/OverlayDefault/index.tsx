942de1f8678bf3fa1f50c1db1eff8c73f31bf6ba
rodrigoaster <rodrigowebx@gmail.com>
2022-12-13 21:40:27 -0300

    OverlayDefault: Changes in component structure and Implementation of animations overlays(title, secondTitle and buttons)

--- src/components/OverlayDefault/index.tsx
+++ src/components/OverlayDefault/index.tsx
@@ -1,5 +1,11 @@
 import * as S from './styles';
 
+import { motion } from 'framer-motion'
+
+import {
+    ContainerAnimate,
+} from "./styles";
+
 interface OverlayDefaultProps {
   firstTitle: string;
   secondTitle: string;
@@ -8,22 +14,56 @@ interface OverlayDefaultProps {
 }
 
 export function OverlayDefault({ firstTitle, secondTitle, buttonText, description }: OverlayDefaultProps) {
-  return (
-    <S.Container>
-      <S.ContainerBlock>
-        <h2>
-          {firstTitle}
-        </h2>
-        <h3>
-          {secondTitle}
-        </h3>
-        <p>
-          {description}
-        </p>
-        <S.Button>
-          {buttonText}
-        </S.Button>
-      </S.ContainerBlock>
-    </S.Container>
-  );
+    const variants =  {
+        variantsSection: {
+            first: { opacity: 0, y: 0 },
+            last: { opacity: 1, y: -50 },
+        },
+        variantsButton: {
+            first: { opacity: 0, y: -35 },
+            last: { opacity: 1, y: -35 },
+        }
+    }
+    return (
+      <S.Container
+          initial="first"
+          whileInView="last"
+          viewport={{ once: true, amount: 0.8 }}
+          variants={variants.variantsSection}
+          transition={{ type: "spring", stiffness: 30 }}
+      >
+        <S.ContainerBlock>
+                <h2>
+                    {firstTitle}
+                </h2>
+            <ContainerAnimate
+                initial="first"
+                whileInView="last"
+                viewport={{ once: true, amount: 0.8 }}
+                variants={variants.variantsSection}
+                transition={{ type: "spring", stiffness: 30, delay: 0.5 }}
+            >
+                <h3>
+                    {secondTitle}
+                </h3>
+                <p>
+                    {description}
+                </p>
+            </ContainerAnimate>
+            <motion.div
+              initial="first"
+              whileInView="last"
+              viewport={{ once: true, amount: 0.8 }}
+              variants={variants.variantsButton}
+              transition={{ type: "spring", stiffness: 30, delay: 1.5 }}
+          >
+            <S.Button>
+                <span>
+                    {buttonText}
+                </span>
+            </S.Button>
+          </motion.div>
+        </S.ContainerBlock>
+      </S.Container>
+    );
 }
--- src/components/OverlayDefault/styles.ts
+++ src/components/OverlayDefault/styles.ts
@@ -1,31 +1,110 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    height: 100vh;
+import {motion} from "framer-motion";
 
+export const Container = styled(motion.div)`
+    height: 100vh;
+  
     display: flex;
     align-items: center;
     justify-content: center;
 `;
 
 export const ContainerBlock = styled.div`
-    > h2 {
-        font-size: 75px;
+    > h2 {       
+        font-size: 70px;
+        letter-spacing: 5px;
     }
 
     > h3 {
-        font-size: 75px;
+        font-size: 70px;
+        letter-spacing: 10px;
     }
-` 
+`
+
+export const ContainerAnimate = styled(motion.div)`
+    h2 {
+      font-size: 70px;
+    }
+
+    > h3 {
+      font-size: 70px;
+      padding-top: 20px;
+    }
+
+    padding-top: 30px;
+`
 
 export const Button = styled.button`
     border: 1px solid #ccc;
-    
-
+  
     padding: 15px 45px;
 
     text-transform: uppercase;
     color: #fff;
 
     font-weight: bold;
-` 
+    letter-spacing: 5px;
+  
+    position: relative;
+  
+    ::before, ::after {
+      content: " ";
+      
+      width: 0;
+      height: 1px;
+      
+      background: #fff;
+      
+      position: absolute;
+
+      transition: 0.2s ease-out ;
+    }
+  
+    ::before {
+      bottom: 0;
+      right: 0;
+    }
+  
+    ::after {
+      top: 0;
+      left: 0;
+    }
+  
+    :hover {
+      ::before, ::after {
+        width: 100%;
+      }
+    }
+  
+    > span {
+      ::before, ::after {
+        content: " ";
+        
+        width: 1px;
+        height: 0;
+        
+        background: #fff;
+        
+        position: absolute;
+
+        transition: 0.2s ease-in ;
+        
+      }
+      
+      ::before {
+        bottom: 0;
+        left: 0;
+      }
+      ::after {
+        right: 0;
+        top: 0;
+      }
+      
+      :hover {
+        ::before, ::after {
+          height: 100%;
+        }
+      }
+    }
+`
